a7a02813ba62d081fd2a733fad6f1fcfbf326ee8
Mirza Abdul Rahman <106723952+mirzarahmansaghir@users.noreply.github.com>
2024-06-01 08:08:42 +0000

    datatypes

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 1
+let accountEmail ="mirzarahmansaghir66@gmail.com"
+var accountPassword ="12345"
+accountCity ="FSD"
+let accountState;
+
+accountEmail ="ma@ma.com"
+accountPassword ="121212"
+accountCity ="LHR"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,14 @@
+"use strict"; 
+console.log(3 + 3);
+
+
+console.log("Mirza");
+
+let name="Mirza"
+let age= 18
+let isLoggedIn =false
+let state;
+
+
+console.log(typeof undefined)
+console.log(typeof null)
